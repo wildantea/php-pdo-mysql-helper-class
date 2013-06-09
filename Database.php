9a288a920ed6d22f129b780b6732ef5fe6636a2b
wildantea <wildannudin@gmail.com>
2013-06-09 20:07:54 +0700

    update indent

--- Database.php
+++ Database.php
@@ -8,213 +8,201 @@
  
 class Database {
   
-        private $pdo;
+    private $pdo;
     
     public function __construct()
     {
-                try {
-                    $this->pdo = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD );
-                    $this->pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-                }
-                catch( PDOException $e ) {
-                    echo "error ". $e->getMessage();
-                }
+        try {
+        $this->pdo = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD );
+        $this->pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch( PDOException $e ) {
+            echo "error ". $e->getMessage();
+        }
     }
-    
-      /**
-       * custom query , joining multiple table, aritmathic etc
-       * @param  string $sql  custom query
-       * @param  array $data associative array 
-       * @return array  recordset 
-       */
-      public function fetch_custom( $sql,$data=null)
-            {
-              if ($data!==null) {
-               $dat=array_values($data);
-              }
-               
-                $sel = $this->pdo->prepare( $sql );
-                if ($data!==null) {
-                    $sel->execute($dat);
-                } else {
-                    $sel->execute();
-                }
-                $sel->setFetchMode( PDO::FETCH_OBJ );
-                return $sel;
-                  
-            }
 
     /**
-     * fetch only one row 
-     * @param  string $table table name
-     * @param  string $col condition column
-     * @param  string $val value column
-     * @return array recordset
-     */
+    * custom query , joining multiple table, aritmathic etc
+    * @param  string $sql  custom query
+    * @param  array $data associative array 
+    * @return array  recordset 
+    */
+    public function fetch_custom( $sql,$data=null) {
+        if ($data!==null) {
+        $dat=array_values($data);
+        }
+        $sel = $this->pdo->prepare( $sql );
+        if ($data!==null) {
+            $sel->execute($dat);
+        } else {
+            $sel->execute();
+        }
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        return $sel;
+    }
+
+    /**
+    * fetch only one row 
+    * @param  string $table table name
+    * @param  string $col condition column
+    * @param  string $val value column
+    * @return array recordset
+    */
     public function fetch_single_row($table,$col,$val)     
     {       
-                $nilai=array($val);
-                $sel = $this->pdo->prepare("SELECT * FROM $table WHERE $col=?");
-                $sel->execute($nilai);
-                $sel->setFetchMode( PDO::FETCH_OBJ );
-                $obj = $sel->fetch();
-                return $obj;
+        $nilai=array($val);
+        $sel = $this->pdo->prepare("SELECT * FROM $table WHERE $col=?");
+        $sel->execute($nilai);
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        $obj = $sel->fetch();
+        return $obj;
     }
 
     /**
-     * fetch all data 
-     * @param  string $table table name
-     * @return array recordset
-     */
+    * fetch all data 
+    * @param  string $table table name
+    * @return array recordset
+    */
     public function fetch_all($table)
     {
-         $sel = $this->pdo->prepare("SELECT * FROM $table");
-                $sel->execute();
-                $sel->setFetchMode( PDO::FETCH_OBJ );
-                return $sel;
+        $sel = $this->pdo->prepare("SELECT * FROM $table");
+        $sel->execute();
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        return $sel;
     }
     /**
-     * fetch multiple row
-     * @param  string $table table name
-     * @param  array $dat specific column selection
-     * @return array recordset
-     */
+    * fetch multiple row
+    * @param  string $table table name
+    * @param  array $dat specific column selection
+    * @return array recordset
+    */
     public function fetch_col($table,$dat)
     {
         if( $dat !== null )
-                        $cols= array_values( $dat );
-                        $col=implode(', ', $cols);
-
-                    $sel = $this->pdo->prepare("SELECT $col from $table");
-                    $sel->execute();
-                    $sel->setFetchMode( PDO::FETCH_OBJ );
-                    return $sel;
+        $cols= array_values( $dat );
+        $col=implode(', ', $cols);
+        $sel = $this->pdo->prepare("SELECT $col from $table");
+        $sel->execute();
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        return $sel;
     }
 
     /**
-     * fetch row with condition
-     * @param  string $table table name
-     * @param  array $col which columns name would be select 
-     * @param  array $where what column will be the condition
-     * @return array recordset
-     */
+    * fetch row with condition
+    * @param  string $table table name
+    * @param  array $col which columns name would be select 
+    * @param  array $where what column will be the condition
+    * @return array recordset
+    */
     public function fetch_multi_row($table,$col,$where)
     {
-         
-                        $data = array_values( $where ); 
-                       //grab keys 
-                        $cols=array_keys($where);
-                        $colum=implode(', ', $col);
 
-                    foreach ($cols as $key) {
-                      $keys=$key."=?";
-                      $mark[]=$keys;
-                    }
-                    
-                      $jum=count($where);
-                          if ($jum>1) {
-                              $im=implode('? and  ', $mark);
-                               $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
-                          } else {
-                            $im=implode('', $mark);
-                               $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
-                          }
-                    $sel->execute( $data );
-                    $sel->setFetchMode( PDO::FETCH_OBJ );
-                   return  $sel;
+        $data = array_values( $where ); 
+        //grab keys 
+        $cols=array_keys($where);
+        $colum=implode(', ', $col);
+        foreach ($cols as $key) {
+          $keys=$key."=?";
+          $mark[]=$keys;
+        }
+
+        $jum=count($where);
+        if ($jum>1) {
+            $im=implode('? and  ', $mark);
+             $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
+        } else {
+          $im=implode('', $mark);
+             $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
+        }
+        $sel->execute( $data );
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        return  $sel;
     }
-    
+
     /**
-     * check if there is exist data
-     * @param  string $table table name 
-     * @param  array $dat array list of data to find
-     * @return true or false
-     */
-    public function check_exist($table,$dat)
-    {
+    * check if there is exist data
+    * @param  string $table table name 
+    * @param  array $dat array list of data to find
+    * @return true or false
+    */
+    public function check_exist($table,$dat) {
 
-                        $data = array_values( $dat ); 
-                       //grab keys 
-                        $cols=array_keys($dat);
-                        $col=implode(', ', $cols);
+        $data = array_values( $dat ); 
+       //grab keys 
+        $cols=array_keys($dat);
+        $col=implode(', ', $cols);
 
-                    foreach ($cols as $key) {
-                      $keys=$key."=?";
-                      $mark[]=$keys;
-                    }
-                    
-                      $jum=count($dat);
-                          if ($jum>1) {
-                              $im=implode(' and  ', $mark);
-                               $sel = $this->pdo->prepare("SELECT $col from $table WHERE $im");
-                          } else {
-                            $im=implode('', $mark);
-                               $sel = $this->pdo->prepare("SELECT $col from $table WHERE $im");
-                          }
-                    $sel->execute( $data );
-                    $sel->setFetchMode( PDO::FETCH_OBJ );
+        foreach ($cols as $key) {
+          $keys=$key."=?";
+          $mark[]=$keys;
+        }
 
-                   
-                    $jum=$sel->rowCount();
-                    if ($jum>0) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                    
+        $jum=count($dat);
+        if ($jum>1) {
+            $im=implode(' and  ', $mark);
+             $sel = $this->pdo->prepare("SELECT $col from $table WHERE $im");
+        } else {
+          $im=implode('', $mark);
+             $sel = $this->pdo->prepare("SELECT $col from $table WHERE $im");
+        }
+        $sel->execute( $data );
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        $jum=$sel->rowCount();
+        if ($jum>0) {
+            return true;
+        } else {
+            return false;
+        }     
     }
     /**
-     * search data
-     * @param  string $table table name
-     * @param  array $col   column name
-     * @param  array $where where condition 
-     * @return array recordset
-     */
-    public function search($table,$col,$where) 
-    {
-             $data = array_values( $where );
-               foreach ($data as $key) {
-                   $val = '%'.$key.'%';
-                   $value[]=$val;
-               }
-                       //grab keys 
-                        $cols=array_keys($where);
-                        $colum=implode(', ', $col);
+    * search data
+    * @param  string $table table name
+    * @param  array $col   column name
+    * @param  array $where where condition 
+    * @return array recordset
+    */
+    public function search($table,$col,$where) {
+        $data = array_values( $where );
+        foreach ($data as $key) {
+           $val = '%'.$key.'%';
+           $value[]=$val;
+        }
+       //grab keys 
+        $cols=array_keys($where);
+        $colum=implode(', ', $col);
 
-                    foreach ($cols as $key) {
-                      $keys=$key." LIKE ?";
-                      $mark[]=$keys;
-                    }
-                    
-                      $jum=count($where);
-                          if ($jum>1) {
-                              $im=implode(' OR  ', $mark);
-                               $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
-                          } else {
-                            $im=implode('', $mark);
-                               $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
-                          }
-                         
-                        $sel->execute($value);
-                        $sel->setFetchMode( PDO::FETCH_OBJ );
-                        return  $sel;
+        foreach ($cols as $key) {
+          $keys=$key." LIKE ?";
+          $mark[]=$keys;
+        }
+        $jum=count($where);
+        if ($jum>1) {
+            $im=implode(' OR  ', $mark);
+             $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
+        } else {
+          $im=implode('', $mark);
+             $sel = $this->pdo->prepare("SELECT $colum from $table WHERE $im");
+        }
+           
+        $sel->execute($value);
+        $sel->setFetchMode( PDO::FETCH_OBJ );
+        return  $sel;
     }
     /**
-     * insert data to table
-     * @param  string $table table name
-     * @param  array $dat   associative array 'column_name'=>'val'
-     */
-   public function insert($table,$dat)
-    {
-        
+    * insert data to table
+    * @param  string $table table name
+    * @param  array $dat   associative array 'column_name'=>'val'
+    */
+    public function insert($table,$dat) {
+
         if( $dat !== null )
-            $data = array_values( $dat );
+        $data = array_values( $dat );
         //grab keys 
         $cols=array_keys($dat);
         $col=implode(', ', $cols);
 
         //grab values and change it value
-          $mark=array();
+        $mark=array();
         foreach ($data as $key) {
           $keys='?';
           $mark[]=$keys;
@@ -222,26 +210,25 @@ class Database {
         $im=implode(', ', $mark);
         $ins = $this->pdo->prepare("INSERT INTO $table ($col) values ($im)");
         $ins->execute( $data );
-       
+
     }
 
     /**
-     * update record
-     * @param  string $table table name
-     * @param  array $dat   associative array 'col'=>'val'
-     * @param  string $id    primary key column name
-     * @param  int $val   key value
-     */
-public function update($table,$dat,$id,$val)
-    {
-         if( $dat !== null )
-            $data = array_values( $dat ); 
+    * update record
+    * @param  string $table table name
+    * @param  array $dat   associative array 'col'=>'val'
+    * @param  string $id    primary key column name
+    * @param  int $val   key value
+    */
+    public function update($table,$dat,$id,$val) {
+        if( $dat !== null )
+        $data = array_values( $dat ); 
         array_push($data,$val);
-          //grab keys
+        //grab keys
         $cols=array_keys($dat);
-          $mark=array();
+        $mark=array();
         foreach ($cols as $col) {
-          $mark[]=$col."=?"; 
+        $mark[]=$col."=?"; 
         }
         $im=implode(', ', $mark);
         $ins = $this->pdo->prepare("UPDATE $table SET $im where $id=?");
@@ -249,27 +236,19 @@ public function update($table,$dat,$id,$val)
     }
 
     /**
-     * delete record
-     * @param  string $table table name
-     * @param  string $where column name for condition (commonly primay key column name)
-     * @param   int $id   key value
-     */
-      public function delete( $table, $where,$id )
-    {
-        
+    * delete record
+    * @param  string $table table name
+    * @param  string $where column name for condition (commonly primay key column name)
+    * @param   int $id   key value
+    */
+    public function delete( $table, $where,$id ) { 
         $data = array( $id ); 
         $sel = $this->pdo->prepare("Delete from $table where $where=?" );
         $sel->execute( $data );
     }
-    public function close()
-    {
-        $this->pdo = null;
-    }
-        
-  
-    public function __destruct()
-    {
-        $this->pdo = null;
+    
+    public function __destruct() {
+    $this->pdo = null;
     }
 }  
 
