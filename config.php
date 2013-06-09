3a61e3dd44799f6dfed18b082d79ddd3ed1bacae
wildantea <wildannudin@gmail.com>
2013-06-09 15:43:16 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+ini_set( "display_errors", true );
+define( "DB_DSN", "mysql:host=localhost;dbname=latihan" );
+define( "DB_USERNAME", "root" );
+define( "DB_PASSWORD", "" );
+
+
+require_once ('Database.php');
+//require_once ('My_pagination.php');
+
+$db=new Database();
+//$pg=New My_pagination();
+
+function handleException( $exception ) {
+  echo  $exception->getMessage();
+}
+
+set_exception_handler( 'handleException' );
+?>
