43b5237dde674687181eebdc536e13d2b984a679
wildantea <wildannudin@gmail.com>
2020-04-09 16:04:16 +0700

    change a few code to accomodate new Database.php

--- config.php
+++ config.php
@@ -1,19 +1,20 @@
 <?php
-ini_set( "display_errors", true );
-define( "DB_DSN", "mysql:host=localhost;dbname=latihan" );
-define( "DB_USERNAME", "root" );
-define( "DB_PASSWORD", "" );
-define('DB_CHARACSET', 'utf8');
+date_default_timezone_set('Asia/Jakarta');
+ini_set("display_errors", true);
 
-require_once ('Database.php');
-//require_once ('My_pagination.php');
+$host = "localhost";
+//$port = 3306;
+$db_username = "root";
+$db_password = "";
+$db_name = "latihan";
 
-$db=new Database();
-//$pg=New My_pagination();
+require_once("Database.php");
 
-function handleException( $exception ) {
-  echo  $exception->getMessage();
+$db=new Database($host, $port, $db_username, $db_password, $db_name);
+
+function handleException($exception)
+{
+    echo  $exception->getMessage();
 }
 
-set_exception_handler( 'handleException' );
-?>
+set_exception_handler('handleException');
